57d629ca2d64a056f229a66f9c642ed644ba520f
DannyFromGuttman <drcjr43@gmail.com>
2024-01-31 11:15:50 -0500

    20240130 H&S class work

--- dec_to_bin.py
+++ dec_to_bin.py
@@ -1,10 +1,10 @@
 def decimal_to_binary(number):
     result = ""
+    number = int(number)
     while number > 0:       # keep dividing until at 0
         remainder = number % 2
         number = number // 2
-        result = remainder + result #place string in reverse order
-        str(remainder)
+        result = str(remainder) + result #place string in reverse
     return result
 
 def main():
